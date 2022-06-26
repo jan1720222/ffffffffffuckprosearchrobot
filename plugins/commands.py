ecb934a9475b06a09ef184f84ec34fb5bb7a3235
jan1720222 <97850205+jan1720222@users.noreply.github.com>
2022-06-27 01:32:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -41,7 +41,7 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
-        await msg.reply("Send MOVIE Name and Year")
+        await message.reply("Send MOVIE Name and Year")
         
         return
     if AUTH_CHANNEL and not await is_subscribed(client, message):
